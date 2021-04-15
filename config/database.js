8b7ac684f5443fb51f4ea60f66983681dcebb061
Jered Nav <jered.nav@icloud.com>
2021-04-14 23:21:38 -0700

    comments added

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
-const mongoose = require('mongoose')
 
+const mongoose = require('mongoose')
+//connects us to mongoDB through mongoose
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB_STRING, {
