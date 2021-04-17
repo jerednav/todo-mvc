be44a641d50de311a3f34ee1ce652068aa250a58
Jered Nav <jered.nav@icloud.com>
2021-04-16 23:25:26 -0700

    server coment addded

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const todoRoutes = require('./routes/todos')
 
 require('dotenv').config({path: './config/.env'})
 
-connectDB()
+connectDB() //callback from connectDB function in the config/databse folder
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
