1062d180e34a19f6137e80c62e3806501060ed62
jerednav <80506833+jerednav@users.noreply.github.com>
2021-04-26 14:46:33 -0700

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,11 @@
+//
+
 const express = require('express')
 const router = express.Router()
 const homeController = require('../controllers/home')
 
 router.get('/', homeController.getIndex) 
+//made a request on the main route, we are gonna talk to the homeController
+//the .getIndex method will render the view EJS to HTML which will respond back to the browser
 
 module.exports = router
