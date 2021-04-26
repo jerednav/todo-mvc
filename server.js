beb957bc96e393b606ac4cf481ab61f23e270d3e
jerednav <80506833+jerednav@users.noreply.github.com>
2021-04-26 14:31:56 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const connectDB = require('./config/database') //requiring database file
+const connectDB = require('./config/database') //requiring database file to run
 const homeRoutes = require('./routes/home')
 const todoRoutes = require('./routes/todos')
 
